b1e842b434a2b23f27a1a68fc9c49352e2542a06
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-04-03 00:16:11 +0300

    initial commit

---
+++ src/test/java/com/nklymok/artstore/ArtstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nklymok.artstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
