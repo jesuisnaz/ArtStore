e2aaa21206932e9e4c74d638915ebe6d098af379
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-04-03 20:03:50 +0300

    fixed circular dependency

--- src/main/java/com/nklymok/artstore/controller/LoginController.java
+++ src/main/java/com/nklymok/artstore/controller/LoginController.java
@@ -1,28 +1,12 @@
 package com.nklymok.artstore.controller;
 
-import com.nklymok.artstore.exception.UserAlreadyExistsException;
-import com.nklymok.artstore.model.User;
-import com.nklymok.artstore.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-
-    private final UserService userService;
-
-    @Autowired
-    public LoginController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping
     public String showLogin() {
