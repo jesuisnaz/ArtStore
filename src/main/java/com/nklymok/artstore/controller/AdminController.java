b829c7c124fee7425b34567ca92975055c1af6eb
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-04-08 22:17:12 +0300

    uploading image to s3 (WIP)

--- src/main/java/com/nklymok/artstore/controller/AdminController.java
+++ src/main/java/com/nklymok/artstore/controller/AdminController.java
@@ -1,12 +1,21 @@
 package com.nklymok.artstore.controller;
 
+import com.nklymok.artstore.service.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public AdminController(StorageService storageService) {
+        this.storageService = storageService;
+    }
 
     @GetMapping
     public String showAdminPanel() {
@@ -16,6 +25,14 @@ public class AdminController {
     @GetMapping("/edit_featured")
     public String showEditFeatured() {
         return "edit_featured";
+    }
+
+    @PostMapping("/add_featured")
+    public String addFeatured(@ModelAttribute("file") MultipartFile file) {
+        if (file.getSize() > 0) {
+            storageService.uploadFile(file, "featured");
+        }
+        return "redirect:/";
     }
 
 }
