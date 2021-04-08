a1e3706652e22ab870c3c7a13f36944ea166f3b1
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-04-08 21:43:31 +0300

    added storage service

---
+++ src/main/java/com/nklymok/artstore/service/StorageService.java
@@ -0,0 +1,55 @@
+package com.nklymok.artstore.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.nklymok.artstore.config.S3Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+@Service
+public class StorageService {
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${s3.bucketName}")
+    private String bucketName;
+
+    @Autowired
+    public StorageService(AmazonS3 amazonS3) {
+        this.amazonS3 = amazonS3;
+    }
+
+    public void uploadFile(MultipartFile multipartFile, String category) {
+         File file = convertMultipartFileToFile(multipartFile);
+         String filename = file.getName();
+         String path = String.format("%s/%s", category, filename);
+         putFileIntoS3(path, file);
+
+    }
+
+    private File convertMultipartFileToFile(MultipartFile multipartFile) {
+        File file = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(file);
+            outputStream.write(multipartFile.getBytes());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    private void putFileIntoS3(String filepath, File file) {
+        amazonS3.putObject(
+                new PutObjectRequest(bucketName, String.format("%s/%s", filepath, file.getName()),
+                        file).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+}
