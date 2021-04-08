6651a3bb30e48adf2eaedab51c2e43cb06f7bba2
Klymok Nazariy <klymok.nazariy@gmail.com>
2021-04-08 21:25:00 +0300

    renamed s3config to s3configuration for codestyle maintenance reasons

--- src/main/java/com/nklymok/artstore/config/S3Configuration.java
+++ src/main/java/com/nklymok/artstore/config/S3Configuration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class S3Config {
+public class S3Configuration {
 
     @Value("${s3.region}")
     private String region;
